27b6642aa354b9a88fb5e2e0a6351ec9d6b68594
chengdui <chengdui19930520@163.com>
2018-11-07 14:21:26 +0800

    解决冲突

--- src/main/resources/com/yyt/Test.java
+++ src/main/resources/com/yyt/Test.java
@@ -3,6 +3,10 @@ package com.yyt;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("aaaaa");
+		
+		System.out.println("123");
+		
+		
 
 		System.out.println("噼噼啪啪");
 		System.out.println("永远永远");
