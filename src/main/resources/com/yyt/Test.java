aac8cf64d22552b66ff73b81ca4eafa00be2b74e
chengdui <chengdui19930520@163.com>
2018-11-07 13:38:23 +0800

    deeeee

--- src/main/resources/com/yyt/Test.java
+++ src/main/resources/com/yyt/Test.java
@@ -3,6 +3,7 @@ package com.yyt;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("aaaaa");
-		System.out.println("2222");
+		System.out.println("25645945");
+		System.out.println("xuxuxuuxu");
 	}
 }
