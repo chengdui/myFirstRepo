052df88f0f5deb0717a274f98fab38f8c9f846ee
chengdui <chengdui19930520@163.com>
2018-11-07 14:01:48 +0800

    111111111111111

--- src/main/resources/com/yyt/Test.java
+++ src/main/resources/com/yyt/Test.java
@@ -4,9 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("aaaaa");
 
-		System.out.println("25645945");
-		System.out.println("xuxuxuuxu");
-
 		System.out.println("2222");
 		System.out.println("xuxuxuuxu");
 	}
